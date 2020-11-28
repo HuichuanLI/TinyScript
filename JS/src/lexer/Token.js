3e0b61ba06c5afefcba64e590f1e043418676bd7
huichuan <lhc14124908@163.com>
2020-11-28 13:47:56 +0800

    add js

---
+++ JS/src/lexer/Token.js
@@ -0,0 +1,45 @@
+class Token {
+    constructor(type, value) {
+      this._type = type;
+      this._value = value;
+    }
+  
+    getType() {
+      return this._type;
+    }
+  
+    getValue() {
+      return this._value;
+    }
+  
+    isVariable() {
+      return this._type == TokenType.VARIABLE;
+    }
+  
+    isValue() {
+      return this.isScalar() || this.isVariable();
+    }
+  
+    isType() {
+      return (
+        this._value === "bool" ||
+        this._value === "int" ||
+        this._value === "float" ||
+        this._value === "void" ||
+        this._value === "string"
+      );
+    }
+  
+    isScalar() {
+      return (
+        this._type == TokenType.INTEGER ||
+        this._type == TokenType.FLOAT ||
+        this._type == TokenType.STRING ||
+        this._type == TokenType.BOOLEAN
+      );
+    }
+  
+    toString() {
+      return `type ${this._type.type}, value ${this._value}`;
+    }
+}  
