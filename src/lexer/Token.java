771645417b8f2b742f06ba5f9a3179590848a335
huichuan <lhc14124908@163.com>
2020-11-28 13:25:58 +0800

    add

---
+++ src/lexer/Token.java
@@ -0,0 +1,39 @@
+package lexer;
+
+/**
+ * Author : lihuichuan
+ * Time   : 2020/11/28
+ */
+public class Token {
+    TokenType _type;
+    String _value;
+
+    public Token(TokenType type, String value) {
+        this._type = type;
+        this._value = value;
+    }
+
+    public TokenType getType() {
+        return _type;
+    }
+
+    public String getValue() {
+        return _value;
+    }
+
+    public boolean isVariable() {
+        return _type == TokenType.VARIABLE;
+    }
+
+    public boolean isScalar() {
+        return _type == TokenType.INTEGER || _type == TokenType.FLOAT ||
+                _type == TokenType.STRING || _type == TokenType.BOOLEAN;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("type %s, value %s", _type, _value);
+    }
+    
+
+}
