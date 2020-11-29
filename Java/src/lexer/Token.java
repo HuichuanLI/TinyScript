6c5d3853fbecbece63d695aa1fee72027c6b448d
huichuan <lhc14124908@163.com>
2020-11-29 14:50:25 +0800

    add

--- Java/src/lexer/Token.java
+++ Java/src/lexer/Token.java
@@ -96,6 +96,163 @@ public class Token {
         throw new LexicalException("Unexpected error");
     }
 
+    public static Token makeOp(PeekIterator<Character> it) throws LexicalException {
+        int state = 0;
+
+        while (it.hasNext()) {
+            char lookahead = it.next();
+
+            switch (state) {
+                case 0:
+                    switch (lookahead) {
+                        case '+':
+                            state = 1;
+                            break;
+                        case '-':
+                            state = 2;
+                            break;
+                        case '*':
+                            state = 3;
+                            break;
+                        case '/':
+                            state = 4;
+                            break;
+                        case '>':
+                            state = 5;
+                            break;
+                        case '<':
+                            state = 6;
+                            break;
+                        case '=':
+                            state = 7;
+                            break;
+                        case '!':
+                            state = 8;
+                            break;
+                        case '&':
+                            state = 9;
+                            break;
+                        case '|':
+                            state = 10;
+                            break;
+                        case '^':
+                            state = 11;
+                            break;
+                        case '%':
+                            state = 12;
+                            break;
+                        case ',':
+                            return new Token(TokenType.OPERATOR, ",");
+                        case ';':
+                            return new Token(TokenType.OPERATOR, ";");
+                    }
+                    break;
+                case 1:
+                    if (lookahead == '+') {
+                        return new Token(TokenType.OPERATOR, "++");
+                    } else if (lookahead == '=') {
+                        return new Token(TokenType.OPERATOR, "+=");
+                    } else {
+                        it.putBack();
+                        return new Token(TokenType.OPERATOR, "+");
+                    }
+                case 2:
+                    if (lookahead == '-') {
+                        return new Token(TokenType.OPERATOR, "--");
+                    } else if (lookahead == '=') {
+                        return new Token(TokenType.OPERATOR, "-=");
+                    } else {
+                        it.putBack();
+                        return new Token(TokenType.OPERATOR, "-");
+                    }
+                case 3:
+                    if (lookahead == '=') {
+                        return new Token(TokenType.OPERATOR, "*=");
+                    } else {
+                        it.putBack();
+                        return new Token(TokenType.OPERATOR, "*");
+                    }
+                case 4:
+                    if (lookahead == '=') {
+                        return new Token(TokenType.OPERATOR, "/=");
+                    } else {
+                        it.putBack();
+                        return new Token(TokenType.OPERATOR, "/");
+                    }
+                case 5:
+                    if (lookahead == '=') {
+                        return new Token(TokenType.OPERATOR, ">=");
+                    } else if (lookahead == '>') {
+                        return new Token(TokenType.OPERATOR, ">>");
+                    } else {
+                        it.putBack();
+                        return new Token(TokenType.OPERATOR, ">");
+
+                    }
+                case 6:
+                    if (lookahead == '=') {
+                        return new Token(TokenType.OPERATOR, "<=");
+                    } else if (lookahead == '<') {
+                        return new Token(TokenType.OPERATOR, "<<");
+                    } else {
+                        it.putBack();
+                        return new Token(TokenType.OPERATOR, "<");
+                    }
+                case 7:
+                    if (lookahead == '=') {
+                        return new Token(TokenType.OPERATOR, "==");
+                    } else {
+                        it.putBack();
+                        return new Token(TokenType.OPERATOR, "=");
+                    }
+                case 8:
+                    if (lookahead == '=') {
+                        return new Token(TokenType.OPERATOR, "!=");
+                    } else {
+                        it.putBack();
+                        return new Token(TokenType.OPERATOR, "!");
+                    }
+                case 9:
+                    if (lookahead == '&') {
+                        return new Token(TokenType.OPERATOR, "&&");
+                    } else if (lookahead == '=') {
+                        return new Token(TokenType.OPERATOR, "&=");
+                    } else {
+                        it.putBack();
+                        return new Token(TokenType.OPERATOR, "&");
+                    }
+                case 10:
+                    if (lookahead == '|') {
+                        return new Token(TokenType.OPERATOR, "||");
+                    } else if (lookahead == '=') {
+                        return new Token(TokenType.OPERATOR, "|=");
+                    } else {
+                        it.putBack();
+                        return new Token(TokenType.OPERATOR, "|");
+                    }
+                case 11:
+                    if (lookahead == '^') {
+                        return new Token(TokenType.OPERATOR, "^^");
+                    } else if (lookahead == '=') {
+                        return new Token(TokenType.OPERATOR, "^=");
+                    } else {
+                        it.putBack();
+                        return new Token(TokenType.OPERATOR, "^");
+                    }
+                case 12:
+                    if (lookahead == '=') {
+                        return new Token(TokenType.OPERATOR, "%=");
+                    } else {
+                        it.putBack();
+                        return new Token(TokenType.OPERATOR, "%");
+                    }
+            }
+
+        }
+        throw new LexicalException("Unexpected error");
+
+    }
+
     @Override
     public String toString() {
         return String.format("type %s, value %s", _type, _value);
