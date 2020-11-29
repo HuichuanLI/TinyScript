c4ce075faca03092baee28f822f477f37d9a1c13
huichuan <lhc14124908@163.com>
2020-11-29 21:31:49 +0800

    add

---
+++ JS/src/lexer/LexicalException.js
@@ -0,0 +1,12 @@
+class LexicalException extends Error{
+    constructor(msg){
+        super(msg)
+    }
+
+    static fromChar(c){
+        return new LexicalException(`unexpected char ${c}`) 
+    }
+
+}
+
+module.exports = LexicalException
