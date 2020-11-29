04fea570136949607b731e84dfa015263e8c63f5
huichuan <lhc14124908@163.com>
2020-11-29 14:52:37 +0800

    add op test

--- Java/test/lexer/TokenTest.java
+++ Java/test/lexer/TokenTest.java
@@ -47,4 +47,29 @@ class TokenTest {
         }
 
     }
+
+    @Test
+    public void test_makeOperator() throws LexicalException {
+        String[] tests = {
+                "+ xxx",
+                "++mmm",
+                "/=g",
+                "==1",
+                "&=3982",
+                "&777",
+                "||xxx",
+                "^=111",
+                "%7"
+        };
+
+        String[] results = {"+", "++", "/=", "==", "&=", "&", "||", "^=", "%"};
+
+        int i = 0;
+        for (String test : tests) {
+
+            PeekIterator it = new PeekIterator<Character>(test.chars().mapToObj(x -> (char) x));
+            Token token = Token.makeOp(it);
+            assertToken(token, results[i++], TokenType.OPERATOR);
+        }
+    }
 }
