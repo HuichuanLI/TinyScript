9e46e1de0f7d63234de9a2dfdfdf5804c5a58ace
huichuan <lhc14124908@163.com>
2020-12-01 20:06:16 +0800

    add

--- JS/src/test/Token.test.js
+++ JS/src/test/Token.test.js
@@ -14,7 +14,6 @@ describe("Token",()=>{
         const it1 = new PeekIterator(arrayToGenerator([..."if abc"]))
         const it2 = new PeekIterator(arrayToGenerator([..."true abc"]))
 
-        console.log(Token)
         const token1 = Token.makeVarOrKeyword(it1)
         const token2 = Token.makeVarOrKeyword(it2)
         it1.next()
@@ -25,4 +24,61 @@ describe("Token",()=>{
         assertToken(token3, "abc", TokenType.VARIABLE)
         
     })
+
+    it("makeString", () =>{
+        const tests = ["'123'", '"123"']
+
+        for(let test of tests) {
+            const it = new PeekIterator(arrayToGenerator([...test]))
+          
+            const token = Token.makeString(it)
+            assertToken(token, test, TokenType.STRING)
+        }
+
+    })
+
+    it("makeOp", () => {
+        const tests = [
+            ["+ xxx", "+"],
+            ["++mmm", "++"],
+            ["/=g", "/="],
+            ["==1", "=="],
+            ["&=3982", "&="],
+            ["&777", "&"],
+            ["||xx", "||"],
+            ["^=111", "^="],
+            ["%7", "%"],
+        ]
+
+        for(let test of tests) {
+            const [input, expected] = test
+            const it = new PeekIterator(arrayToGenerator([...input]))
+            const token = Token.makeOp(it)
+            assertToken(token, expected, TokenType.OPERATOR)
+        }
+    })
+
+    it("makeNumber", () => {
+        const tests = [
+            "+0 aa",
+            "-0 bbb",
+            ".3 ccc",
+            ".5555 ddd",
+            "7899.999 aaa",
+            "-100 ggg",
+            "-1000.5345345*123123",
+            "012 aaa"
+        ]
+
+        for(let test of tests) {
+            const it = new PeekIterator(arrayToGenerator([...test]))
+            const token = Token.makeNumber(it)
+            const [expected] = test.split(/[ *]/);
+            const type = test.indexOf('.') == -1 ? TokenType.INTEGER : TokenType.FLOAT
+            assertToken(token, expected, type)
+        }
+
+
+    })
+
 })
