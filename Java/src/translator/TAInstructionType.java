df57528021030b2e1156e63d6e2df17462506163
huichuan <lhc14124908@163.com>
2020-12-11 15:57:00 +0800

    add

---
+++ Java/src/translator/TAInstructionType.java
@@ -0,0 +1,17 @@
+package translator;
+
+/**
+ * Author : lihuichuan
+ * Time   : 2020/12/11
+ */
+public enum TAInstructionType {
+    ASSIGN,
+    GOTO,
+    IF,
+    LABEL,
+    CALL,
+    RETURN,
+    SP,
+    PARAM,
+    FUNC_BEGIN
+}
